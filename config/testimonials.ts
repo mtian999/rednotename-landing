2aeb53585caf53324bfacb7596201e52121f2381
tianbao <mtian999@163.com>
2025-01-18 20:04:00 +0800

    init:初始化

---
+++ config/testimonials.ts
@@ -0,0 +1,74 @@
+export const TestimonialsData = [
+  {
+    user: {
+      name: "Alex Johnson",
+      username: "top4ai",
+      image: "/images/users/top4ai.jpeg",
+    },
+    content:
+      "I was looking for a chinese name for RedNote and this tool is amazing! It's super easy to use, and the names it generates are fantastic.  I highly recommend it to anyone wanting a chinese name!",
+  },
+  {
+    user: {
+      name: "Sarah Thompson",
+      username: "top4ai",
+      image: "/images/users/1.png",
+    },
+    content:
+      "This chinese name generator is so convenient! I love how it provides options with explanations, which makes choosing a name much easier and helps me understand the meanings.",
+  },
+  {
+    user: {
+      name: "Michael Chen",
+      username: "top4ai",
+      image: "/images/users/2.png",
+    },
+    content:
+      "I've tried other name generators, but this one is specifically tailored for RedNote, which makes it perfect. It's efficient and easy, I found my favorite name in a minute.",
+  },
+  {
+    user: {
+      name: "Emily Rodriguez",
+      username: "top4ai",
+      image: "/images/users/3.png",
+    },
+    content:
+      "The tool is incredibly intuitive and offers a great variety of names. It's also really helpful how they explain the cultural context of each name!",
+  },
+  {
+    user: {
+      name: "David Wilson",
+      username: "top4ai",
+      image: "/images/users/4.png",
+    },
+    content:
+      "I found this to be the most effective tool for generating Chinese names. I love how it takes into account the different style preferences. It's so efficient!",
+  },
+  {
+    user: {
+      name: "Jessica Lee",
+      username: "top4ai",
+      image: "/images/users/5.png",
+    },
+    content:
+      "This chinese name generator is a must-have for anyone looking for a good chinese name for RedNote. It is simple to use, and very effective in finding the right name!",
+  },
+  {
+    user: {
+      name: "Daniel Brown",
+      username: "top4ai",
+      image: "/images/users/4.png",
+    },
+    content:
+      "The user interface is so clean and easy to navigate, it’s incredibly convenient and quick to generate a list of chinese names, and they all feel authentic!",
+  },
+  {
+    user: {
+      name: "Olivia Martinez",
+      username: "top4ai",
+      image: "/images/users/5.png",
+    },
+    content:
+      "I’m impressed by the quality of this generator. It really understands the nuances of Chinese names, and provides really good choices to consider. It’s very practical to use!",
+  },
+];
